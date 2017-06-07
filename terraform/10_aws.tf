44527bf8432c2f3efe20fbe9947efcc65765b809
Andrey Adamovich <andrey@aestasit.com>
2017-06-07 15:54:09 +0300

    created variable for default region

--- terraform/10_aws.tf
+++ terraform/10_aws.tf
@@ -1,8 +1,12 @@
 
+variable "aws_region" {
+  default  = "us-east-1"
+}
+
 provider "aws" {
   access_key = "${var.aws_access_key_id}"
   secret_key = "${var.aws_secret_key}"
-  region     = "us-east-1"
+  region     = "${var.aws_region}"
 }
 
 data "aws_availability_zones" "available" {
