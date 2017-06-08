1a0ace6c68baf206650eeadf383d22678706ff27
Andrey Adamovich <andrey@aestasit.com>
2017-06-08 10:21:15 +0300

    added bucket policy for billing reports

--- terraform/40_buckets.tf
+++ terraform/40_buckets.tf
@@ -11,6 +11,41 @@ resource "aws_s3_bucket" "dev_events_billing" {
   region                  = "${var.aws_region}"
 }
 
+resource "aws_s3_bucket_policy" "dev_events_billing_policy" {
+  bucket                  = "${aws_s3_bucket.dev_events_billing.id}"
+  policy                  = <<EOF
+{
+  "Version": "2008-10-17",
+  "Id": "Policy1335892530063",
+  "Statement": [
+    {
+      "Sid": "Stmt1335892150622",
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": "arn:aws:iam::386209384616:root"
+      },
+      "Action": [
+        "s3:GetBucketAcl",
+        "s3:GetBucketPolicy"
+      ],
+      "Resource": "arn:aws:s3:::dev-events-billing"
+    },
+    {
+      "Sid": "Stmt1335892526596",
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": "arn:aws:iam::386209384616:root"
+      },
+      "Action": [
+        "s3:PutObject"
+      ],
+      "Resource": "arn:aws:s3:::dev-events-billing/*"
+    }
+  ]
+}
+EOF
+}
+
 resource "aws_s3_bucket" "dev_events_logs" {
   bucket                  = "dev-events-logs"
   acl                     = "private"
