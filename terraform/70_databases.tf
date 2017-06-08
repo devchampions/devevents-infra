8f8c6bcb292d1254f5c9398e680dc01a0a777011
Andrey Adamovich <andrey@aestasit.com>
2017-06-08 10:06:54 +0300

    apply db changes immediately

--- terraform/70_databases.tf
+++ terraform/70_databases.tf
@@ -13,6 +13,8 @@ resource "aws_db_instance" "dev_events_db" {
   username               = "master"
   password               = "${var.db_password}"
 
+  apply_immediately      = true
+
   vpc_security_group_ids = [ "${aws_security_group.dev_events_security.id}" ]
   db_subnet_group_name   = "${aws_db_subnet_group.default.name}"
 
