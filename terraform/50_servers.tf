43d7c759b99a96c1d162225609698aa2d11e4e3e
Andrey Adamovich <andrey@aestasit.com>
2017-06-08 12:52:43 +0300

    added all bits needed for codedeploy to work (hopefully)

--- terraform/50_servers.tf
+++ terraform/50_servers.tf
@@ -4,9 +4,59 @@ resource "aws_key_pair" "dev_events_key" {
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDqln5qiHnd8NLz60GOGW/Rqv25rK78yiG5kyv2LeBBe0JVVoJ/BOA7T/BuW/GhuT12SE2o4ZiRlgchdzMld4AaAAwTCUC/A5RdcogjktrMAda9UQnDLwfmohJmAskC6ibq05oqDb3RWfIik2psjJJkZasJVI0OuwDf1DpXjDVjId9y2xkM9sVGjzz8ApM/J6YWVEZY0n2V5qT8Xi5orDQgwtrxrEDcpN5AldC5KnovohUqbCyZSKwenWJ4Rq5uM2rccQR4wMWT0aHh8Sf8k1OTdQqEqc86MBNZKhP4ICOfnBb3/U6DIfC6aK6qDjMqXqU3ojyCmQWu0x3N1sF9tay5 admins@dev.events"
 }
 
+resource "aws_iam_role" "code_deploy_instance" {
+  name               = "code_deploy_instance"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_policy" "code_deploy_instance" {
+  name   = "code_deploy_instance_policy"
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "s3:Get*",
+        "s3:List*"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_policy_attachment" "code_deploy_instance" {
+  name       = "code_deploy"
+  roles      = ["${aws_iam_role.code_deploy_instance.name}"]
+  policy_arn = "${aws_iam_policy.code_deploy_instance.arn}"
+}
+
+resource "aws_iam_instance_profile" "code_deploy_instance" {
+  name  = "code_deploy"
+  role  = "${aws_iam_role.code_deploy_instance.name}"
+}
+
 resource "aws_instance" "dev_events_server" {
   ami = "${data.aws_ami.base_image.id}"
   instance_type = "t2.micro"
+  iam_instance_profile = "${aws_iam_instance_profile.code_deploy_instance.name}"
   tags {
     Name = "dev_events_api"
   }
---
+++ terraform/99_deploy.tf
@@ -0,0 +1,46 @@
+
+resource "aws_codedeploy_app" "dev_events_backend" {
+  name = "dev_events_backend"
+}
+
+resource "aws_codedeploy_deployment_config" "dev_events_backend" {
+  deployment_config_name = "dev_events_backend_deployment"
+  minimum_healthy_hosts {
+    type  = "HOST_COUNT"
+    value = 1
+  }
+}
+
+resource "aws_iam_role" "code_deployer" {
+  name = "code_deployer"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "codedeploy.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_codedeploy_deployment_group" "dev_events_backend" {
+
+  app_name               = "${aws_codedeploy_app.dev_events_backend.name}"
+  deployment_group_name  = "dev-events-deployment-group"
+  service_role_arn       = "${aws_iam_role.code_deployer.arn}"
+  deployment_config_name = "${aws_codedeploy_deployment_config.dev_events_backend.id}"
+
+  ec2_tag_filter {
+    key   = "Name"
+    type  = "KEY_AND_VALUE"
+    value = "dev_events_api"
+  }
+
+}
