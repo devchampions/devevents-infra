2f7ffcf2902736d85ed2a8bc52bec5d781632aff
Andrey Adamovich <andrey@aestasit.com>
2017-06-08 10:06:36 +0300

    added buckets for logging and billing

--- terraform/40_buckets.tf
+++ terraform/40_buckets.tf
@@ -5,3 +5,15 @@ resource "aws_s3_bucket" "dev_events_code" {
   region                  = "${var.aws_region}"
 }
 
+resource "aws_s3_bucket" "dev_events_billing" {
+  bucket                  = "dev-events-billing"
+  acl                     = "private"
+  region                  = "${var.aws_region}"
+}
+
+resource "aws_s3_bucket" "dev_events_logs" {
+  bucket                  = "dev-events-logs"
+  acl                     = "private"
+  region                  = "${var.aws_region}"
+}
+
