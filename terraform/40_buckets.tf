c7d3b37665a83069084b54684a1368a5608f34d1
Andrey Adamovich <andrey@aestasit.com>
2017-06-09 23:11:54 +0300

    added more permissions to logging bucket

--- terraform/40_buckets.tf
+++ terraform/40_buckets.tf
@@ -48,7 +48,7 @@ resource "aws_s3_bucket_policy" "dev_events_billing_policy" {
         "s3:GetBucketAcl",
         "s3:GetBucketPolicy"
       ],
-      "Resource": "arn:aws:s3:::dev-events-billing"
+      "Resource": "${aws_s3_bucket.dev_events_billing.arn}"
     },
     {
       "Sid": "Stmt1335892526596",
@@ -59,7 +59,7 @@ resource "aws_s3_bucket_policy" "dev_events_billing_policy" {
       "Action": [
         "s3:PutObject"
       ],
-      "Resource": "arn:aws:s3:::dev-events-billing/*"
+      "Resource": "${aws_s3_bucket.dev_events_billing.arn}/*"
     }
   ]
 }
@@ -72,3 +72,38 @@ resource "aws_s3_bucket" "dev_events_logs" {
   region                  = "${var.aws_region}"
 }
 
+resource "aws_s3_bucket_policy" "dev_events_logs_policy" {
+  bucket                  = "${aws_s3_bucket.dev_events_logs.id}"
+  policy                  = <<EOF
+{
+  "Version": "2008-10-17",
+  "Id": "Policy1335892530063",
+  "Statement": [
+    {
+      "Sid": "Stmt1335892150622",
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": "arn:aws:iam::386209384616:root"
+      },
+      "Action": [
+        "s3:GetBucketAcl",
+        "s3:GetBucketPolicy",
+        "s3:PutBucketAcl"
+      ],
+      "Resource": "${aws_s3_bucket.dev_events_logs.arn}"
+    },
+    {
+      "Sid": "Stmt1335892526596",
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": "arn:aws:iam::386209384616:root"
+      },
+      "Action": [
+        "s3:PutObject"
+      ],
+      "Resource": "${aws_s3_bucket.dev_events_logs.arn}/*"
+    }
+  ]
+}
+EOF
+}
